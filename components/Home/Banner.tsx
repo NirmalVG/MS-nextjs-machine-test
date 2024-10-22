4ec37b5e36ce7a9bbf350a7b9fe788ae0116e6c9
Nirmal V G <nirmalvg97@gmail.com>
2024-10-22 17:52:36 +0530

    fixes

--- components/Home/Banner.tsx
+++ components/Home/Banner.tsx
@@ -1,11 +1,11 @@
 const Banner = () => {
     return (
         <section className="relative bg-banner h-screen bg-cover bg-center">
-            <div className="container px-4 text-white pt-[550px] md:pt-96 lg:pl-[50px] pl-[50px] w-full lg:w-[900px]">
-                <h2 className="text-[32px] md:text-[44px] lg:text-[54px] font-bold">
+            <div className="container px-4 text-white pt-[450px] md:pt-96 lg:pl-[50px] pl-[50px] w-full lg:w-[900px]">
+                <h2 className="text-[22px] md:text-[44px] lg:text-[54px] font-bold">
                     Lorem ipsum dolor sit amet
                 </h2>
-                <p className="mt-4 text-[14px] md:text-[16px] lg:text-[18px]">
+                <p className="mt-4 text-[10px] md:text-[16px] lg:text-[18px]">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                     Donec mattis velit nulla. In hac habitasse platea dictumst.
                     Fusce non venenatis velit. Proin luctus malesuada mauris
